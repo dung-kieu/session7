fdfbeb8159fdeae7cfa88b95c952140a7d009de8
dung-kieu <Velvet99>
2019-10-03 21:33:03 +0700

    Vẽ hình tròn đơn giản với thông số ngẫu nhiên .

--- funnyCricle/circle.js
+++ funnyCricle/circle.js
@@ -6,8 +6,8 @@ function MyCircle(x,y,radius) {
 
 function createCircle( ) {
     let ctx =document.getElementById("myCanvas").getContext("2d");
-    var radius = Math.floor(Math.random() * 80);
-    var circle= new MyCircle(500, 500, radius);
+    let radius = Math.floor(Math.random() * 80);
+    let circle= new MyCircle(500, 500, radius);
     ctx.beginPath();
     ctx.arc(circle.x, circle.y, circle.radius ,0,2*Math.PI);
     ctx.fill();
