02ab56c3e7ad2850249e2752ee46fa240a90ebfb
dung-kieu <Velvet99>
2019-10-03 17:42:56 +0700

    Vẽ hình tròn đơn giản với thông sso cố định.

---
+++ funnyCricle/circle.js
@@ -0,0 +1,14 @@
+function MyCircle(x,y,radius) {
+    this.x = x;
+    this.y = y;
+    this.radius =radius;
+}
+
+function createCircle( ) {
+    let ctx =document.getElementById("myCanvas").getContext("2d");
+    let circle=new MyCircle(500,500,80);
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius ,0,2*Math.PI);
+    ctx.fill();
+}
+createCircle();
